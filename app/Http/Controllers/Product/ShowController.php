add96a7b4d70be3204ec901aa17ac016105cfa0d
s1llhoutte <danikval2000002@gmail.com>
2025-09-25 16:19:45 +0300

    incomplete crud product

---
+++ app/Http/Controllers/Product/ShowController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Product;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\Tag;
+use Illuminate\Http\Request;
+
+class ShowController extends Controller
+{
+    public function __invoke(Product $product)
+    {
+        return view('product.show', compact('product'));
+    }
+}
